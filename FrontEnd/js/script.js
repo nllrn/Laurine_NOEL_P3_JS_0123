89d5dc5a17ffe43ad756f1fe24c0e55a2e5cb840
nl.lrn <noel.laurine.pro@outlook.fr>
2023-02-04 21:21:56 +0100

    essaie

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -25,7 +25,6 @@ function getAllWorks(allWorks) {
         const image = document.createElement("img");
         image.setAttribute("crossorigin", "anonymous");
         image.src = projects.imageUrl;
-
         const title = document.createElement("figcaption");
         title.innerText = projects.title;
 
@@ -38,5 +37,76 @@ function getAllWorks(allWorks) {
     
 };
 
-console.log(allWorks);
+// console.log(allWorks);
 getAllWorks(allWorks);
+
+const objectBtn = document.querySelector(".objectBtn");
+
+objectBtn.addEventListener("click", function() {
+    const objectCategory = allWorks.filter(function (allWorks) {
+        return allWorks.category.id === 1;
+    });
+    document.querySelector(".objectBtn").innerHTML = "";
+    getAllWorks(objectCategory);
+    console.log(objectCategory, 'objets');
+});
+
+const apartmentBtn = document.querySelector(".apartmentBtn");
+
+apartmentBtn.addEventListener("click", function() {
+    const apartmentCategory = allWorks.filter(function (allWorks) {
+        return allWorks.category.id === 2;
+    });
+    document.querySelector(".apartmentBtn").innerHTML = "";
+    getAllWorks(apartmentCategory);
+    console.log(apartmentCategory, 'appartements');
+});
+
+const hotelBtn = document.querySelector(".hotelBtn");
+
+hotelBtn.addEventListener("click", function() {
+    const hotelCategory = allWorks.filter(function (allWorks) {
+        return allWorks.category.id === 3;
+    });
+    document.querySelector(".hotelBtn").innerHTML = "";
+    getAllWorks(hotelCategory);
+    console.log(hotelCategory, 'hotels');
+});
+
+// let categories = window.localStorage.getItem("categories");
+
+// if (categories === null) {
+//     const answer = await fetch("http://localhost:5678/api/categories");
+//     const categories = await answer.json();
+
+//     const valeurCategories = JSON.stringify(categories);
+//     window.localStorage.setItem("categories", valeurCategories);
+// }else{
+//     categories = JSON.parse(categories);
+// };
+
+// function getCategories(getCategories){
+//     for (let i = 0; i < getCategories.length; i++) {
+
+//         const categories = getCategories[i];
+
+//         // const allBtn = document.querySelector(".allBtn");
+//         // allBtn.innerText = categories.category;
+
+//         const objectBtn = document.querySelector(".objectBtn");
+//         objectBtn.innerText = categories.categoryId = 1;
+//     }
+// };
+
+// getCategories(categories);
+
+// const objectBtn = document.querySelector(".objectBtn");
+
+// objectBtn.addEventListener("click", function(e) {
+//     const objectCategory = categories.filter(function (categories) {
+//         return categories.categoryId = 1;
+//     });
+//     document.querySelector(".objectBtn").innerHTML = "";
+//     getCategories(objectCategory);
+//     console.log(objectCategory, 'tas encore du boulot ma fille');
+// });
--- FrontEnd/readmeup.md
+++ FrontEnd/readmeup.md
@@ -1,7 +1,39 @@
-category : {id: 1, name: 'Objets'}
-categoryId :1
-id : 1
-imageUrl : "http://localhost:5678/images/abajour-tahina1651286843956.png"
-title : "Abajour Tahina"
-userId : 1
-[[Prototype]]: Object
+# infos connexion
+{
+  "email": "string" (sophie.bluel@test.tld),
+  "password": "string" (S0phie)
+}
+{
+  "userId": 1,
+  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4"
+}
+
+
+# infos projets
+{
+    "id": 1,
+    "title": "Abajour Tahina",
+    "imageUrl": "http://localhost:5678/images/abajour-tahina1651286843956.png",
+    "categoryId": 1,
+    "userId": 1,
+    "category": {
+      "id": 1,
+      "name": "Objets"
+    }
+},
+
+# infos catégories
+[
+  {
+    "id": 1,
+    "name": "Objets"
+  },
+  {
+    "id": 2,
+    "name": "Appartements"
+  },
+  {
+    "id": 3,
+    "name": "Hotels & restaurants"
+  }
+]
