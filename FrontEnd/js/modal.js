6b9ccc8be0f3264b345a4eb400f83dfc049b1834
nl.lrn <noel.laurine.pro@outlook.fr>
2023-02-14 21:59:24 +0100

    modification du login et ajout du système de l'interface modal

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -1,13 +1,33 @@
-// A FAIRE POUR ENVOYER DES PROJETS DANS LA GALLERY DANS LA MMODALE
-// function login() {
-//     const login = document.querySelector(".login");
-//     login.addEventListener("submit", function (event) {
-//     event.preventDefault();
+let modal = null;
 
-//     // création de l'utilisateur
-//     const user = {
-//         email : event.target.querySelector("[name=email]").value,
-//         password : event.target.querySelector("[name=password]").value,
-//     };
-//     });
-// }
+const openModal = function (e) {
+    e.preventDefault();
+    const target = document.querySelector(e.target.getAttribute('href'));
+    target.style.display = null;
+    target.removeAttribute('aria-hidden');
+    target.setAttribute('aria-modal', 'true');
+    modal = target;
+    modal.addEventListener("click", closeModal);
+    modal.querySelector('.js-modal-close').addEventListener("click", closeModal);
+    modal.querySelector('.js-modal-stop').addEventListener("click", stopPropagation);
+};
+
+const closeModal = function (e) {
+    if (modal === null) return
+    e.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute('aria-hidden', "true");
+    modal.removeAttribute('aria-modal');
+    modal.removeEventListener("click", closeModal);
+    modal.querySelector('.js-modal-close').removeEventListener("click", closeModal);
+    modal.querySelector('.js-modal-stop').removeEventListener("click", stopPropagation);
+    modal = null;
+};
+
+const stopPropagation = function (e) {
+    e.stopPropagation()
+};
+
+document.querySelectorAll(".js-modal").forEach(a => {
+    a.addEventListener("click", openModal);
+});
