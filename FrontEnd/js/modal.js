26ff634039c086e731fb59675408a1222592b360
nl.lrn <noel.laurine.pro@outlook.fr>
2023-02-17 23:39:31 +0100

    ajout de la modal et des projets dans la modal / début fonction 'DELETE' des projets de l'API

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -31,3 +31,94 @@ const stopPropagation = function (e) {
 document.querySelectorAll(".js-modal").forEach(a => {
     a.addEventListener("click", openModal);
 });
+
+let allWorksModal = window.localStorage.getItem("allWorksModal");
+
+if (allWorksModal ===  null) {
+    const reponse = await fetch("http://localhost:5678/api/works");
+    const allWorksModal = await reponse.json();
+
+    const valeurAllWorksModal = JSON.stringify(allWorksModal);
+    window.localStorage.setItem("allWorksModal", valeurAllWorksModal);
+}else{
+    allWorksModal = JSON.parse(allWorksModal);
+};
+
+function getAllWorksModal(allWorksModal) {
+    for (let i = 0; i < allWorksModal.length; i++) {
+
+        const projectsModal = allWorksModal[i];
+
+        const sectionGalleryModal = document.querySelector(".gallery-modal");
+
+        const figureModal = document.createElement("figure");
+
+        const deleteWorkBtn = document.createElement("button");
+        deleteWorkBtn.classList.add("deleteWorkBtn");
+        // deleteBtn.innerText = 'supprimer';
+
+        const deleteSpanWork = document.createElement("span");
+        deleteSpanWork.innerText = 'supprimer'; 
+
+        const iconTrashcan = document.createElement("i");
+        iconTrashcan.classList.add("fa-solid", "fa-trash-can")
+        
+
+        const imageModal = document.createElement("img");
+        imageModal.setAttribute("crossorigin", "anonymous");
+        imageModal.src = projectsModal.imageUrl;
+
+        const titleModal = document.createElement("figcaption");
+        titleModal.innerText = 'éditer'
+
+        sectionGalleryModal.appendChild(figureModal);
+
+        figureModal.appendChild(imageModal);
+        figureModal.appendChild(deleteWorkBtn);
+        deleteWorkBtn.appendChild(iconTrashcan);
+        deleteWorkBtn.appendChild(deleteSpanWork);
+        figureModal.appendChild(titleModal);
+        
+    }
+};
+
+getAllWorksModal(allWorksModal);
+
+
+// const deleteWork = document.querySelector(".deleteWorkBtn");
+
+// deleteWork.addEventListener("click", function(e) {
+//     const deleteOneWork = 
+// })
+
+
+
+
+
+// const deleteOneWork = document.querySelector(".deleteWorkBtn");
+
+// // deleteWorkBtn.localStorage.getItem(allWorksModal);
+
+// // let deleteOneWork = document.querySelector(".deleteWorkBtn");
+
+// deleteOneWork.addEventListener("click", async function (e) {
+//     e.preventDefault();
+
+    
+//     //window.localStorage.getItem("deleteOneWork");
+
+//     if (deleteOneWork === null) {
+//         const reponse = await fetch(`http://localhost:5678/api/works/${id}`, {
+//             method: "DELETE",
+//             headers: {'Authorization': `Bearer ${userToken}`},
+//             body : JSON.stringify(deleteOneWork),
+//         });
+
+//         const deleteOneWork = await reponse.json();
+//     }else{
+//         alert('raté')
+//     };
+
+//     window.localStorage.removeItem(`allWorksModal${id}`);
+    
+// });
